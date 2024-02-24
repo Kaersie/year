0adb500f383524f5350e5be3b3258b1440986cc9
Your Name <you@example.com>
2024-02-24 17:06:14 +0800

    'new'

--- change.js
+++ change.js
@@ -1,33 +1,36 @@
-﻿function readname() {
-    let url = location.search;
+﻿
+
+  function readname(){
+    let url=location.search;
     let strs;
     let str;
     let name;
-    if (url.indexOf("?") != -1) {
-        str = url.substr(1);
-        strs = str.split("&");
-        name = decodeURIComponent(strs[0].replace("name=", ""));
-        thing = decodeURIComponent(strs[1].replace("thing=", ""));
+    if(url.indexOf("?")!=-1){
+        str=url.substr(1);
+        strs=str.split("&");
+        name=decodeURIComponent(strs[0].replace("name=",""));
+        thing=decodeURIComponent(strs[1].replace("thing=",""));
     }
-    if (name == "" | name == null | name == undefined) {
-        window.location.href = "./share.html";
-    } else if (thing == "" | thing == null | name == undefined) {
-        thing = "期待有美好的事情发生"
+    if (name==""| name==null|name==undefined){
+       window.location.href="./share.html";
     }
-    let aa = [name, thing];
+    else if(thing==""|thing==null|name==undefined){
+        thing="期待有美好的事情发生"
+    }
+    let aa=[name,thing];
     return aa;
 }
 
 
 
-function showit() {
-    let first1 = document.getElementById("first");
+function showit(){
+    let first1=document.getElementById("first");
     console.log(first1);
-    let a = readname();
+    let a=readname();
     console.log(a);
-    first1.innerText = "致 " + a[0] + " :";
-    let b = document.getElementById("second");
-    b.innerText = a[1];
+    first1.innerText="致 "+a[0]+" :";
+    let b=document.getElementById("second");
+    b.innerText=a[1];
 
 }
 showit();
